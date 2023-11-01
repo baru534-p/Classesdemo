b6fee82664e228a692215a894e2bbfa6f028c404
TavvaBhargavi <baru.tavva@gmail.com>
2023-11-01 11:17:07 +0530

    Add project files.

---
+++ classlibrarydemo1/Subject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlibrarydemo1
+{
+    public class Subject
+    {
+        private string name;
+        private string shortName;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+            set { shortName = value; }
+        }
+
+    }
+}
